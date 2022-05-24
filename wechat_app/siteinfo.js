85acf3fb11c8202ef267b1426542ececbdb7f72c
likecu <254067848@qq.com>
2022-05-24 22:44:36 +0800

    添加上传文件界面

--- wechat_app/siteinfo.js
+++ wechat_app/siteinfo.js
@@ -7,6 +7,3 @@ module.exports = {
     siteroot: "https://www.dartshop.xyz/app/index.php",
     design_method: "2"
 };
-//需要更多源码请关注微信公众号“玩家手稿”，点击下方手稿源码进入微信小程序查看全部演示。
-//进入源码详情，点击“去体验”即可打开演示版小程序
-//演示后台：wq.jwjw888.com  账号：admin    密码：123456
